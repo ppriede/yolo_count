fa125732dad6200e23d5c239598ab05480d7dd66
Patricio Priede <17771721+ppriede@users.noreply.github.com>
2023-03-29 23:31:43 -0300

    Update p.py
    Se mejora el procesamiento en tiempo real, faltaba strem=True para un correcto procesamiento de videos largos.
    Puede ser que en Yolo CLI tambien afecte.

--- p.py
+++ p.py
@@ -1,13 +1,28 @@
 import os
+import argparse
 from ultralytics import YOLO
 import supervision as sv
 import numpy as np
+
+
 # Carpeta de trabajo
 HOME = os.getcwd()
+# Despues seguimos mirando las instrucciones de argparse
+# https://machinelearningmastery.com/command-line-arguments-for-your-python-script/
+# parser = argparse.ArgumentParser(description="Proceso de video con Ultralytics YOLOv8 + Supervision",
+#                                  formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+# parser.add_argument("entrada", help="Entrade de Imagen (JPG, PNG, MP4, MKV, etc)", default=f"{HOME}/../seg1_corto.mp4")
+# args = parser.parse_args()
+# config = vars(args)
+# print(config)
+# print(config.entrada)
+# exit()
+
 # Video para analizar
-SOURCE_VIDEO_PATH = f"{HOME}/../seg3.mkv"
+SOURCE_VIDEO_PATH = f"{HOME}/../seg1_corto.mp4"
+# TARGET_VIDEO_PATH = cfg.source if cfg.source is not None else TARGET_VIDEO_PATH
 # Video resultado final
-TARGET_VIDEO_PATH = f"{HOME}/seg3_yolo_count.mkv"
+TARGET_VIDEO_PATH = f"{HOME}/seg1_corto_stream_T_yolo_count.mp4"
 # Linea de deteccion
 LINE_START = sv.Point(122, 518)
 LINE_END = sv.Point(516, 487)
@@ -17,6 +32,7 @@ print(video_info)
 duracion = video_info.total_frames/video_info.fps
 print("Duracion:",duracion,"segundos")
 #exit()
+
 def main():
     line_counter = sv.LineZone(
         start=LINE_START,
@@ -31,24 +47,28 @@ def main():
         text_scale=1
     )
     model = YOLO("yolov8n.pt")
-    i=0
+    i=0 # Cuadro inicial
+    # Si calculamos el tiempo, obtenemos el tiempo del cuadro
+    # i/fps
     with sv.VideoSink(TARGET_VIDEO_PATH, video_info) as sink:
         for result in model.track(
-            source=SOURCE_VIDEO_PATH,
-            save=False,
-            save_txt=False,
-            save_conf=False,
-            save_crop=False,
-            hide_labels=True,
-            hide_conf=True,
-            visualize=False,
-            boxes=False,
-            show=False,
-            conf=0.25,
-            device=0,
-            classes=[2,3,5,6,7],
-            verbose=True):
-            i += 1
+            source=SOURCE_VIDEO_PATH, # Ruta de archivo a leer, si es 0 es webcam o camara conectada
+            save=False, # Guarda video
+            save_txt=False, # Guarda la informacion del cuadro donde se detecto algo
+            save_conf=False, # Guarda el intervalo de confianza junto con los datos
+            save_crop=False, # Guarda una imagen de lo detectado
+            hide_labels=True, # Oculta la etiqueta de la deteccion
+            hide_conf=True, # Oculta el intervalo de confianza en la imagen
+            visualize=False, # No se
+            boxes=False, # Muestra las cajas alrededor del objeto
+            show=False, # Si puede, muestra informacion
+            conf=0.25, # Intervalo de confianza para la deteccion
+            device=0, # Usamos device=0 (GPU)
+            classes=[2,3,5,6,7], # Solo identificamos estas clases (coco.txt con lista completa)
+            verbose=False, #Esto genera mas informacion en CLI
+            stream=True # Esto, al parecer, entrega los datos en cada deteccion... veamos si lo entiendo bien
+            ):
+            tiempo =i/video_info.fps
             frame = result.orig_img
             detections = sv.Detections.from_yolov8(result)
             if result.boxes.id is not None:
@@ -63,9 +83,10 @@ def main():
                 detections=detections,
                 labels=labels)
             line_counter.trigger(detections=detections)
-            print("IN: ",line_counter.in_count, "i: ",i)
+            print("IN: ",line_counter.in_count, "i: ",i,"Tiempo: ",tiempo,"segundos")
             line_annotator.annotate(frame=frame, line_counter=line_counter)
             sink.write_frame(frame=frame)
+            i+=1
 
 
 if __name__ == "__main__":
